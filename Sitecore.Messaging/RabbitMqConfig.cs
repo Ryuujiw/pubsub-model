f649f3870f4e2baf90bf738ce4495d769751da50
rujinw <rujinwong@gmail.com>
2023-03-07 23:42:15 +0800

    Add Messaging project

---
+++ Sitecore.Messaging/RabbitMqConfig.cs
@@ -0,0 +1,9 @@
+﻿namespace Sitecore.Messaging
+{
+    public class RabbitMqConfig
+    {
+        public string Hostname { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
